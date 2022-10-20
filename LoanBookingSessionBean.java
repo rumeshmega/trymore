cac3a3979c0212de6feb177bde0f58eba8ea6e6f
“rumeshmega” <rumeshmega98@gmail.com>
2022-10-20 05:37:35 +0000

    thirdone

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -11,12 +11,12 @@ public class SpiralPatternExample2
 public static void printPattern(int n)  
 {  
 //detrmines the boundary size of the array  
-int size = 2 * n - 8;  
+int size = 5 * n - 8;  
 //inner loop  
-for(int i = 1; i <= size; j++)  
+for(int i = 1; i <= size; i++)  
 {  
 //outer loop      
-for(int j = 1; j <= size; j++)  
+for(int j = 1; i <= size; j++)  
 {  
 //calculates and prints the values for pattern  
 System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
